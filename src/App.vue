a7b383bee0731f5942a7fb40357eaf8de720bc2c
fkn1017 <kosuke.fukano@gmail.com>
2020-05-12 14:08:12 +0900

    顔検出領域を描画する処理を追加

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,12 @@
         prepend-icon="mdi-camera"
         placeholder="画像をドラッグ＆ドロップするか選択してください。"
       ></v-file-input>
-      <v-btn class="my-2" color="primary" :disabled="!file" block
+      <v-btn
+        class="my-2"
+        color="primary"
+        :disabled="!file"
+        block
+        @click="detectFaces(file)"
         >Detect!!</v-btn
       >
       <v-row justify="center" align="center">
@@ -20,6 +25,8 @@
 </template>
 
 <script>
+import detectFaces from "./libs/visionApi";
+
 export default {
   name: "App",
   data: () => ({
@@ -28,9 +35,11 @@ export default {
       (value) =>
         !value ||
         value.size < 2000000 ||
-        "Avatar size should be less than 2 MB!",
+        "Image size should be less than 2 MB!",
     ],
     canvasWidth: 800,
+    imgWidth: 800,
+    results: {},
   }),
   methods: {
     addDropFile(e) {
@@ -45,9 +54,9 @@ export default {
         const img = new window.Image();
         img.src = url;
         img.onload = () => {
+          this.imgWidth = img.width;
           const canvas = this.$refs.preview;
           const context = canvas.getContext("2d");
-          console.log(canvas.width);
           if (img.width > canvas.width) {
             const scale = canvas.width / img.width;
             const sw = img.width;
@@ -67,6 +76,66 @@ export default {
       const canvas = this.$refs.preview;
       const context = canvas.getContext("2d");
       context.clearRect(0, 0, canvas.width, canvas.height);
+    },
+    detectFaces: async function() {
+      try {
+        const data = await detectFaces(this.file);
+
+        if (data && data.responses) {
+          const faceAnnotations = data.responses[0].faceAnnotations;
+          const boundingPolies = [];
+          faceAnnotations.forEach((annotation) => {
+            boundingPolies.push(annotation.boundingPoly);
+          });
+          this.results = boundingPolies;
+          this.drawRects();
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    getRects() {
+      let rects = [];
+
+      this.results.forEach((poly) => {
+        const xArray = poly.vertices.map((v) => v.x);
+        const yArray = poly.vertices.map((v) => v.y);
+
+        const xmin = Math.min.apply(Math, xArray);
+        const xmax = Math.max.apply(Math, xArray);
+        const ymin = Math.min.apply(Math, yArray);
+        const ymax = Math.max.apply(Math, yArray);
+        const width = xmax - xmin;
+        const height = ymax - ymin;
+
+        rects.push({
+          x: xmin,
+          y: ymin,
+          width,
+          height,
+        });
+      });
+
+      return rects;
+    },
+    drawRects() {
+      const canvas = this.$refs.preview;
+      const context = canvas.getContext("2d");
+      const rects = this.getRects();
+      context.strokeStyle = "rgb(0, 255, 0)";
+      context.lineWidth = 2;
+      rects.forEach((rect) => {
+        let scale = 1;
+        if (this.imgWidth > canvas.width) {
+          scale = canvas.width / this.imgWidth;
+        }
+        context.strokeRect(
+          rect.x * scale,
+          rect.y * scale,
+          rect.width * scale,
+          rect.height * scale
+        );
+      });
     },
   },
 };
